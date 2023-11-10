729d180ac56c6f12743e040723e97def8c291adb
klausTT <1171403318@qq.com>
2023-11-10 09:44:59 +0800

    init

---
+++ xc/iosFramework.xcframework/ios-arm64_x86_64-simulator/iosFramework.framework/Headers/iosFramework.h
@@ -0,0 +1,16 @@
+//
+//  iosFramework.h
+//  iosFramework
+//
+//  Created by dongtao wu on 2023/11/7.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iosFramework.
+FOUNDATION_EXPORT double iosFrameworkVersionNumber;
+
+//! Project version string for iosFramework.
+FOUNDATION_EXPORT const unsigned char iosFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iosFramework/PublicHeader.h>
